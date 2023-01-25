7adc0b63da3e02cb4eef00aaf56f202e7b4432f4
cnieusma <cnieusma@uchicago.edu>
2023-01-25 22:04:47 +0000

    subtraction page

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,7 +1,11 @@
 class MathController < ApplicationController
 
   def minus
-    render({ :template => "math_templates/subtraction.html.erb" })
+    #render({ :template => "math_templates/subtraction.html.erb" })
+
+    
+
+    render({ :template => "math_templates/minus_form.html.erb" })
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/", { :controller => "math", :action => "minus" })
 
-  get("/math_templates/subtract", { :controller => "math", :action => "minus" })
+  get("/subtract", { :controller => "math", :action => "minus" })
 
 end

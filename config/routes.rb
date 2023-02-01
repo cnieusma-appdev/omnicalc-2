9522b27c917ec400502b41d3e2dddaf2eefe04cf
cnieusma <cnieusma@uchicago.edu>
2023-02-01 02:44:59 +0000

    change names for wizard

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   get("/", { :controller => "math", :action => "minus" })
 
   get("/subtract", { :controller => "math", :action => "minus" })
-  get("/wizard_subtraction", { :controller => "math", :action => "subtraction" })
+  get("/wizard_subtract", { :controller => "math", :action => "subtraction" })
 
   get("/add", { :controller => "math", :action => "add" })
-  get("/wizard_addition", { :controller => "math", :action => "addition" })
+  get("/wizard_add", { :controller => "math", :action => "addition" })
 
   get("/multiply", { :controller => "math", :action => "multiply" })
-  get("/wizard_multiplication", { :controller => "math", :action => "multiplication" })
+  get("/wizard_multiply", { :controller => "math", :action => "multiplication" })
 
   get("/divide", { :controller => "math", :action => "divide" })
-  get("/wizard_division", { :controller => "math", :action => "division" })
+  get("/wizard_divide", { :controller => "math", :action => "division" })
 
 end
